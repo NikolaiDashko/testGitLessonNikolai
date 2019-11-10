625a47619fe82db52362028a211d5ee193212683
Nikolai Dashko <dashko28@gmail.com>
2019-11-10 23:12:59 +0200

    Zadaniya delayu

---
+++ src/HomeWork/PI_pyat_raz.java
@@ -0,0 +1,13 @@
+package HomeWork;
+
+public class PI_pyat_raz {
+
+		public static void main(String[] args) {
+			
+			System.out.println(String.format("%.1f%n", Math.PI));	// 1 цифра после запятой
+			System.out.println(String.format("%.2f%n", Math.PI));	// 2 цифры после запятой
+			System.out.println(String.format("%.3f%n", Math.PI));	// 3 цифры после запятой
+			System.out.println(String.format("%.4f%n", Math.PI));	// 4 цифры после запятой
+			System.out.println(String.format("%.5f%n", Math.PI));	// 5 цифры после запятой
+		}
+	}
--- src/Program1.java
+++ src/Program1.java
@@ -3,7 +3,7 @@ public class Program1 {
 
 	   public static void main(String[] args) {
 	     
-	      Scanner sc = new Scanner(System.in);
+	      Scanner sc = new Scanner(System.in);{
     System.out.println("Вычислим площадь треугольника.");
     System.out.println("Введите первую сторону треугольника : ");
     double a = sc.nextDouble();
@@ -13,5 +13,7 @@ public class Program1 {
     double c = sc.nextDouble();
     double p = (a + b + c)/2;
     System.out.println("Площадь треугольника равна : " + Math.sqrt(p * (p - a) * (p - b) * (p - c)));
+}
+	   sc.close();
 }
 }

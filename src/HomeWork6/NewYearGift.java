994e8783becb87051a242aaab68a7ad86d8f7a8d
Nikolai Dashko <dashko28@gmail.com>
2019-11-21 16:51:52 +0200

    Started HW6

---
+++ src/HomeWork6/NewYearGift.java
@@ -0,0 +1,36 @@
+package HomeWork6;
+import java.util.*;
+
+public class NewYearGift {
+    private ArrayList<Candy> candies = new ArrayList<Candy>();
+
+    public Box(ArrayList<Candy> candies) {
+        this.candies = candies;
+    }
+
+    //в порядку зростання
+    public ArrayList<Candy> sortCandysByWeigth(){
+        Collections.sort(candies);
+        return candies;
+    }
+
+
+    public Candy getCandyFromCaloriesRange(double fromCalories, double toCalories){
+        for(Candy candy: candies){
+            if (candy.getCalories() >= fromCalories &&
+                    candy.getCalories()<= toCalories)
+                return candy;
+        }
+        throw new RuntimeException();
+    }
+
+
+    @Override
+    public String toString() {
+        String result = "Box: " + "\n";
+        for(Candy candy: candies){
+            result += candy.toString()+ "\n";
+        }
+        return result;
+    }
+}
